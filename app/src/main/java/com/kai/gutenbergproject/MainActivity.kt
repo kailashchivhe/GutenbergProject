ce97a420d9b47c303af7ebb347dc9b511a44fa3d
Kailash Chivhe <kchivhe@gmail.com>
2021-03-12 09:16:16 +0530

    feat ( Toolbar ) : Added custom toolbar support and added kotlin extensions support.

--- app/src/main/java/com/kai/gutenbergproject/MainActivity.kt
+++ app/src/main/java/com/kai/gutenbergproject/MainActivity.kt
@@ -3,12 +3,15 @@ package com.kai.gutenbergproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kai.gutenbergproject.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar( toolbar )
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
